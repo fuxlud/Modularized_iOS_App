afd069cab2a656575e7e56fb43ff46a172b1ef09
fuxlud <fuxlud@gmail.com>
2024-06-22 20:18:25 +0200

    more mock data

--- Recipes/Application/RecipesApp.swift
+++ Recipes/Application/RecipesApp.swift
@@ -3,12 +3,14 @@ import Domain
 import SearchScreen
 import Networking
 import Data
+import DomainMock
 @main
 
 struct RecipesApp: App {
     var body: some Scene {
         WindowGroup {
-            let repository = RandomRecipesRepository(service: WebService())
+//            let repository = RandomRecipesRepository(service: WebService())
+            let repository = RandomRecipesRepositoryMock()
             let viewModel = SearchRecipesViewModel(repository: repository)
             SearchRecipesView(viewModel: viewModel)
         }
