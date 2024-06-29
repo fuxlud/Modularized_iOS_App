416f8ec6da7e1ed28c7aac24bc7db55a3266bec8
Luda Fux <fuxlud@gmail.com>
2024-06-29 13:33:01 +0200

    Use Case tests

---
+++ Submoduls/Sources/DomainTests/RandomRecipesUseCaseTests.swift
@@ -0,0 +1,31 @@
+import Testing
+import Domain
+import DomainMock
+
+struct RandomRecipesUseCaseTests {
+    
+    @Test func whenFetchRecipiesSuccess_shouldReturnRecipiesList() async {
+        let sut = RandomRecipesUseCaseMock(recipes: RecipeEntity.mock)
+        
+        do {
+            let recipies = try await sut.getRandomRecipes()
+            
+            #expect(recipies.count == 20)
+            
+            if let recipy = recipies.first {
+                #expect(recipy.title == "Traditional spare ribs baked")
+            }
+        } catch {}
+    }
+    
+    @Test func whenFetchRecipiesFailure_shouldReturnRecipiesError() async {
+        let sut = RandomRecipesUseCaseMock(error: RecipyErrorEntity.general)
+        
+        do {
+            let _ = try await sut.getRandomRecipes()
+        } catch let error {
+            guard let concreteError = error as? RecipyErrorEntity else { return }
+            #expect(concreteError == RecipyErrorEntity.general)
+        }
+    }
+}
