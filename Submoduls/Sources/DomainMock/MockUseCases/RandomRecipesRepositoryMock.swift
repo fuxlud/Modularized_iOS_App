a57631b1d0a3f28556cf48336d35fe27cdfd034f
Luda Fux <fuxlud@gmail.com>
2024-06-29 13:26:58 +0200

    failure test for use case

--- Submoduls/Sources/DomainMock/MockUseCases/RandomRecipesRepositoryMock.swift
+++ Submoduls/Sources/DomainMock/MockUseCases/RandomRecipesRepositoryMock.swift
@@ -2,17 +2,17 @@ import Domain
 
 public struct RandomRecipesUseCaseMock: RandomRecipesUseCaseProtocol {
     
-    let recipes: [Domain.RecipeEntity]
+    let recipes: [Domain.RecipeEntity]?
     let error: Error?
     
     public func getRandomRecipes() async throws -> [Domain.RecipeEntity] {
         if let error {
             throw error
         }
-        return recipes
+        return recipes ?? []
     }
     
-    public init(recipes: [Domain.RecipeEntity], error: Error? = nil) {
+    public init(recipes: [Domain.RecipeEntity]? = nil, error: Error? = nil) {
         self.recipes = recipes
         self.error = error
     }
--- Submoduls/Sources/DomainTests/UseCaseTest.swift
+++ Submoduls/Sources/DomainTests/UseCaseTest.swift
@@ -4,7 +4,7 @@ import DomainMock
 
 final class UseCaseTest: XCTestCase {
 
-    func testWhenFetchAstronomiesSuccess_shouldReturnAstronomiesList() async  {
+    func testWhenFetchRecipiesSuccess_shouldReturnRecipiesList() async  {
         let sut = RandomRecipesUseCaseMock(recipes: RecipeEntity.mock)
 
         do {
@@ -20,6 +20,20 @@ final class UseCaseTest: XCTestCase {
 
         } catch {
             XCTFail("Fetching astronomies should return results")
+        }
+    }
+    
+    func testWhenFetchRecipiesFailure_shouldReturnRecipiesError() async  {
+        let sut = RandomRecipesUseCaseMock(error: RecipyErrorEntity.general)
+
+        do {
+            let _ = try await sut.getRandomRecipes()
+        } catch let error {
+            guard let concreteError = error as? RecipyErrorEntity else {
+                XCTFail("The error should be of RecipyErrorEntity type")
+                return
+            }
+            XCTAssertEqual(concreteError, RecipyErrorEntity.general)
         }
     }
 }
