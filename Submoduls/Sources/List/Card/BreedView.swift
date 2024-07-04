78d0e078d1410b03d005f68ddadd1a495d694465
Luda Fux <fuxlud@gmail.com>
2024-07-04 15:03:02 +0200

    3 columns grid

--- Submoduls/Sources/List/Card/BreedView.swift
+++ Submoduls/Sources/List/Card/BreedView.swift
@@ -10,16 +10,11 @@ public struct BreedView: View {
   }
   
   public var body: some View {
-    VStack(alignment: .leading) {
       Text(viewModel.title)
         .font(.headline)
-        .lineLimit(2)
-        .frame(maxWidth: .infinity, maxHeight: .infinity ,alignment: .leading)
-    }
-    .padding([.leading, .trailing, .bottom])
-    .background(Color.white)
-    .cornerRadius(8)
-    .shadow(color: Color(.black).opacity(0.2), radius: 5, x: 0, y: 5)
-    .frame(maxWidth: .infinity, maxHeight: .infinity) // Ensure the view takes the maximum space allowed
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+        .background(Color.white)
+        .cornerRadius(8)
+        .shadow(color: Color(.black).opacity(0.2), radius: 5, x: 0, y: 5)
   }
 }
