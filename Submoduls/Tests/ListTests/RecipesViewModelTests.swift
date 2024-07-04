a961872923e864e41cd7b952b123d36b6596c2a2
Luda Fux <fuxlud@gmail.com>
2024-07-04 13:36:11 +0200

    ViewModel tests fixed

--- Submoduls/Tests/ListTests/RecipesViewModelTests.swift
+++ Submoduls/Tests/ListTests/RecipesViewModelTests.swift
@@ -5,25 +5,25 @@ import List
 struct RecipesViewModelTests {
     
     @Test func whenRecipesFetchedSuccesfully_shouldFillRecipesViewModelArray_andShowCorrectInfo() async {
-        let sut = await makeSUT(randomRecipesUseCase: RandomRecipesUseCaseMock(recipes: RecipeEntity.mock))
+        let sut = await makeSUT(breedsUseCase: BreedsUseCaseMock(breeds: BreedEntity.mock))
         await sut.dispatch(.onAppear)
         
-        await #expect(sut.state.data?.count == 20)
-        await #expect(sut.state.error == nil)
+        #expect(sut.state.data?.count == 10)
+        #expect(sut.state.error == nil)
         
-        if let recipeViewModel = await sut.state.data?.first as? RecipeEntity {
-            #expect(recipeViewModel.title == "Traditional spare ribs baked")
+        if let breedViewModel = sut.state.data?.first as? BreedEntity {
+            #expect(breedViewModel.name == "Golden Retriever")
         }
     }
     
     @Test func whenAstronomiesFetchError_shouldPresentError() async {
-        let sut = await makeSUT(randomRecipesUseCase: RandomRecipesUseCaseMock(error: RecipeErrorEntity.general))
+        let sut = await makeSUT(breedsUseCase: BreedsUseCaseMock(error: ErrorEntity.general))
         
         await sut.dispatch(.onAppear)
         
-        await #expect(sut.state.error != nil)
+        #expect(sut.state.error != nil)
         
-        if let error = await sut.state.error {
+        if let error = sut.state.error {
             #expect(error == "Oops, something went wrong")
         }
     }
@@ -31,8 +31,8 @@ struct RecipesViewModelTests {
     
     // MARK: - Test helpers
     @MainActor
-    func makeSUT(randomRecipesUseCase: some RandomRecipesUseCaseProtocol) -> RecipesViewModel {
-        let sut = RecipesViewModel(randomRecipesUseCase: randomRecipesUseCase)
+    func makeSUT(breedsUseCase: some BreedsUseCaseProtocol) -> BreedsViewModel {
+        let sut = BreedsViewModel(breedsUseCase: breedsUseCase)
         return sut
     }
 }
