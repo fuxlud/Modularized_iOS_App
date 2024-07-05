7efc6301ba7cd004f11fb085d403aafa9889c3aa
Luda Fux <fuxlud@gmail.com>
2024-07-05 17:26:45 +0200

    Image details preview

--- Submoduls/Sources/PresentationLayer_Features_DetailsScreen/DetailCard/DetailsCard.swift
+++ Submoduls/Sources/PresentationLayer_Features_DetailsScreen/DetailCard/DetailsCard.swift
@@ -12,15 +12,47 @@ public struct DetailsCard: View {
     }
     
     public var body: some View {
-        Group {
-            if let imageUrl = viewModel.imageUrl {
-                URLImage(imageUrl)
-                    .frame(width: tileSize, height: tileSize)
-                    .listRowSeparator(.hidden)
+        ZStack {
+            Group {
+                if let imageUrl = viewModel.imageUrl {
+                    URLImage(imageUrl)
+                        .frame(width: tileSize, height: tileSize)
+                        .listRowSeparator(.hidden)
+                }
             }
+            .background(Color.white)
+            .cornerRadius(8)
+            .shadow(color: Color(.black).opacity(0.2), radius: 5, x: 0, y: 5)
+            
+            favoriteButton
         }
-        .background(Color.white)
-        .cornerRadius(8)
-        .shadow(color: Color(.black).opacity(0.2), radius: 5, x: 0, y: 5)
+    }
+    
+    var favoriteButton: some View {
+        VStack {
+            HStack {
+                Spacer()
+                Button(action: {
+                  ()
+                }) {
+                  Image(systemName: "star")
+                    .font(.system(size: 24))
+                    .foregroundColor(.white)
+                }
+            }
+            .padding(10)
+            Spacer()
+        }
+        .padding(5)
+    }
+}
+
+struct DetailsCard_Preview: PreviewProvider {
+    static var previews: some View {
+        let breedDetails = BreedDetailsEntity.mock.first
+        let viewModel = DetailsCardViewModel(imageDetails: breedDetails!)
+        
+        return DetailsCard(viewModel: viewModel, tileSize: 100)
+            .previewLayout(.sizeThatFits)
     }
 }
---
+++ Submoduls/Sources/PresentationLayer_Features_DetailsScreen/DetailCard/DetailsCard_Preview.swift
@@ -0,0 +1,7 @@
+//
+//  DetailsCard_Preview.swift
+//  
+//
+//  Created by Luda Fux on 05/07/2024.
+//
+
