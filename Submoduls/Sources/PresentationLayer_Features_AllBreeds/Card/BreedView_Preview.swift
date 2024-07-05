5a62fe1871574b9599723dbe3fc9212e4f35e02c
Luda Fux <fuxlud@gmail.com>
2024-07-05 17:32:15 +0200

    Placig the previews in Views

--- Submoduls/Sources/PresentationLayer_Features_AllBreeds/Card/BreedView_Preview.swift
+++ Submoduls/Sources/PresentationLayer_Features_AllBreeds/Card/BreedView_Preview.swift
@@ -2,12 +2,12 @@ import SwiftUI
 import PresentationLayer_DesignSystem
 import DomainLayer
 
-struct BreedView_Preview: PreviewProvider {
-    static var previews: some View {
-        let breedMock = BreedEntity.mock.first
-        let viewModel = BreedViewModel(breed: breedMock!)
-        
-        return BreedView(viewModel: viewModel)
-            .previewLayout(.sizeThatFits)
-    }
-}
+//struct BreedView_Preview: PreviewProvider {
+//    static var previews: some View {
+//        let breedMock = BreedEntity.mock.first
+//        let viewModel = BreedViewModel(breed: breedMock!)
+//        
+//        return BreedView(viewModel: viewModel)
+//            .previewLayout(.sizeThatFits)
+//    }
+//}
