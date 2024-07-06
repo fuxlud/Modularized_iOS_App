386c4c78b5b022a7e39131dce365e627b58c7d5c
Luda Fux <fuxlud@gmail.com>
2024-07-06 21:16:09 +0200

    Mesh View

---
+++ Submoduls/Sources/PresentationLayer_DesignSystem/ColorfulMeshGradient.swift
@@ -0,0 +1,32 @@
+import SwiftUI
+
+public struct ColorfulMeshGradient: View {
+    
+    public init() {}
+    
+    public var body: some View {
+        Group {
+            if #available(iOS 18.0, *) {
+                MeshGradient(width: 2, height: 2, points: [
+                    [0, 0], [1, 0], [0, 1], [1, 1]
+                ], colors: getRandomColors())
+            } else {
+                LinearGradient(gradient: Gradient(colors: getRandomColors()), startPoint: .topLeading, endPoint: .bottomTrailing)
+            }
+        }
+    }
+    
+    func getRandomColors() -> [Color] {
+      var randomColors: [Color] = []
+        let colors = ThemePallet.colors
+      if colors.count >= 4 {
+        randomColors = Array(colors.shuffled().prefix(4))
+      }
+      return randomColors
+    }
+    
+}
+
+#Preview {
+    ColorfulMeshGradient()
+}
