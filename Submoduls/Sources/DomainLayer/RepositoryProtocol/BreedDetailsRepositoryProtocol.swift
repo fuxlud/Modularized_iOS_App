27f2c441d8d15543574efe7888fd93216d99b8d5
Luda Fux <fuxlud@gmail.com>
2024-07-08 00:16:24 +0200

    combine to reactivly respond to likes in favorites screen

--- Submoduls/Sources/DomainLayer/RepositoryProtocol/BreedDetailsRepositoryProtocol.swift
+++ Submoduls/Sources/DomainLayer/RepositoryProtocol/BreedDetailsRepositoryProtocol.swift
@@ -1,7 +1,9 @@
+import Combine
 import Foundation
 
 public protocol BreedDetailsRepositoryProtocol {
     func getRemoteBreedDetails(breedName: String) async throws -> [BreedDetailsEntity]
-    func toggleLiking(breedDetailsEntity: BreedDetailsEntity) 
+    func toggleLiking(breedDetailsEntity: BreedDetailsEntity)
     func fetchFavorites() async -> Set<BreedDetailsEntity>
+    var itemsPublisher: AnyPublisher<[BreedDetailsEntity], Never> { get }
 }
